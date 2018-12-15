027aa3381b762956fe07795cce6dd68d71b84cae
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-12-15 11:55:33 +0530

    emit new message on create message event

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', {
-		from: 'John',
-		text: 'Hey. It\'s a mail for you.',
-		createAt: 123456
-	});
-
-	socket.on('createMessage', (newMessage) => {
-		console.log(newMessage);
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
