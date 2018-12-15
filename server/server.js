28efd3d56cc79c75456878a33b41baed1fc1ae76
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-12-15 23:21:18 +0530

    Create genrateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path    = require('path');
 const http    = require('http');
 const socketIO  = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,"../public");
 const port = process.env.PORT || 3000;
 const app = express();
@@ -15,27 +16,15 @@ io.on('connection', (socket) => {
 	console.log('New user connected');
 
 	// socket.emit from Admin to new User text welcome to the chat app
-	socket.emit('newMessage', {
-		from: 'Admin',
-		text: 'Welcome to the chat app',
-		createdAt: new Date().getTime()
-	});
-	
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
 	//everyone will see new user joined except new user
-	socket.broadcast.emit('newMessage', {
-		from: 'Admin',
-		text: 'New User joined',
-		createdAt: new Date().getTime()
-	});	
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));	
 
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 		// every one will get this message.
-		// io.emit('newMessage', {
-		// 	from: message.from,
-		// 	text: message.text,
-		// 	createdAt: new Date().getTime()
-		// });
+		io.emit('newMessage',generateMessage(message.from, message.text));
 
 		//With this the sender will not see the message, but everyone else will receive the message.
 		// socket.broadcast.emit('newMessage', {
@@ -45,7 +34,7 @@ io.on('connection', (socket) => {
 		// });
 	});
 
-	socket.on('disconnect', () => {
+	 socket.on('disconnect', () => {
 			console.log('User disconnected.');
 	});
 });
